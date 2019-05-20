8e2fadc516ab986b29a9606a4bb29e168bbb5001
akmui <akmui0912@gmail.com>
2019-05-20 20:44:58 +0900

    .

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const PORT = 4000;
 const handleListening = () => console.log(`listening on:http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send('Hi from home!');
 const handleProfile = (req, res) => res.send("Hi this is my profile");
+const handleProfile2 = (req, res) => res.send("Yay");
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
+app.get("/profile2", handleProfile2);
 app.listen(PORT, handleListening);

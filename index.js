1a2a1df8e4c4d74188a9bfdf16fe60d369bd8442
akmui <akmui0912@gmail.com>
2019-05-28 19:25:38 +0900

    .

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser form "bodyParser";
+
+
 var app = express();
 
 const PORT = 4000;
@@ -6,6 +12,13 @@ const handleListening = () => console.log(`listening on:http://localhost:${PORT}
 const handleHome = (req, res) => res.send('Hi from home!');
 const handleProfile = (req, res) => res.send("Hi this is my profile");
 const handleProfile2 = (req, res) => res.send("Yay");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);

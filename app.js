39a6f98080b7f160a57d516c104187badcae3f89
akmui <akmui0912@gmail.com>
2019-05-28 20:00:35 +0900

    .

--- app.js
+++ app.js
@@ -4,14 +4,10 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 
-
 var app = express();
 
-const PORT = 4000;
-const handleListening = () => console.log(`listening on:http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send('Hi from home!');
 const handleProfile = (req, res) => res.send("Hi this is my profile");
-const handleProfile2 = (req, res) => res.send("Yay");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -22,5 +18,5 @@ app.use(helmet());
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
-app.get("/profile2", handleProfile2);
-app.listen(PORT, handleListening);
+
+export default app;

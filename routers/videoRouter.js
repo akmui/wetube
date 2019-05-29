4b659b099bbd9ebede1a11f53b3cbc16936a68a4
akmui <akmui0912@gmail.com>
2019-05-29 15:34:41 +0900

    .

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,7 +3,7 @@ import routes from "../routes";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, (req, res) => res.send("Videos"));
+videoRouter.get("/", (req, res) => res.send("Videos"));
 videoRouter.get(routes.upload, (req, res) => res.send("Upload"));
 videoRouter.get(routes.videoDetail, (req, res) => res.send("Video detail"));
 videoRouter.get(routes.editVideo, (req, res) => res.send("Edit video"));

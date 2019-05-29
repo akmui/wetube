108fa97033d43dbb44b3ff98e34b0d49380f51b4
akmui <akmui0912@gmail.com>
2019-05-29 13:53:03 +0900

    routes

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router"
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
+
 
 var app = express();
-
-const handleHome = (req, res) => res.send('Hi from home!');
-const handleProfile = (req, res) => res.send("Hi this is my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -16,9 +17,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(helmet());
 
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.video, videoRouter);
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-app.use("/user", userRouter);
 
 export default app;

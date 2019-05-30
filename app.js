324c9827b256c507bf6d9defb1ae95669714673c
akmui <akmui0912@gmail.com>
2019-05-30 20:16:21 +0900

    .

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
-
 var app = express();
 
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import routes from "../routes";
+import { videos, upload, videoDetail, editVideo, deleteVideo } from "../controllers/videoControlloer";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/", (req, res) => res.send("Videos"));
-videoRouter.get(routes.upload, (req, res) => res.send("Upload"));
-videoRouter.get(routes.videoDetail, (req, res) => res.send("Video detail"));
-videoRouter.get(routes.editVideo, (req, res) => res.send("Edit video"));
-videoRouter.get(routes.deleteVideo, (req, res) => res.send("Delete video"));
+videoRouter.get("/", videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;

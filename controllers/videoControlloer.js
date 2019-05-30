f8b45bfb3097a0edf9d49171f16c8256263eaaa3
akmui <akmui0912@gmail.com>
2019-05-30 20:26:03 +0900

    .

--- controllers/videoControlloer.js
+++ controllers/videoControlloer.js
@@ -1,7 +1,7 @@
 export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
+export const search = (req, res) => res.send("search");
+export const videos = (req, res) => res.send("videos");
 export const upload = (req, res) => res.send("upload");
-export const videoDetail = (req, res) => res.send("Video Details");
-export const editVideo = (req, res) => res.send("Edit Videos");
-export const deleteVideo = (req, res) => res.send("Delete Videos");
+export const videoDetail = (req, res) => res.send("videoDetail");
+export const editVideo = (req, res) => res.send("editVideo");
+export const deleteVideo = (req, res) => res.send("deleteVideo");

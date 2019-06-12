6ba260be5306e205b552b95835cd2602f935ed7c
akmui <akmui0912@gmail.com>
2019-06-12 18:27:13 +0900

    .

--- controllers/videoControlloer.js
+++ controllers/videoControlloer.js
@@ -1,4 +1,6 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+import {videoDb} from "../db";
+
+export const home = (req, res) => res.render("home", { pageTitle: "Home", videoDb });
 export const search = (req, res) => {
     const { 
         query: { term: searchingBy } 
---
+++ db.js
@@ -0,0 +1,38 @@
+export const videoDb = [
+    {
+        id:324293,
+        title:'video awesome',
+        description:'This is something I love',
+        views:24,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator:{
+            id:121212,
+            name:"akmui",
+            email:"akmui@akmui.com"
+        }
+    },
+    {
+        id:123123,
+        title:'video nice',
+        description:'This is something I love',
+        views:24,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator:{
+            id:121212,
+            name:"akmui",
+            email:"akmui@akmui.com"
+        }
+    },  
+    {
+        id:15656,
+        title:'video perfect',
+        description:'This is something I love',
+        views:24,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator:{
+            id:121212,
+            name:"akmui",
+            email:"akmui@akmui.com"
+        }
+    }
+];

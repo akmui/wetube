eda43b4537092512d853f44ed4378be0d084dcaa
akmui <akmui0912@gmail.com>
2019-06-26 22:33:29 +0900

    6/26 final

--- models/Video.js
+++ models/Video.js
@@ -1,22 +1,27 @@
 import mongoose from "mongoose";
 
 const VideoSchema = new mongoose.Schema({
-    fileUrl:{
+    fileUrl: {
         type: String,
-        required: `File URL is required`},
+        required: `File URL is required`
+    },
     title: {
         type: String,
-        requried: `Title is required`},
-    description: String, 
+        requried: `Title is required`
+    },
+    description: String,
     views: {
         type: Number,
-        default: 0},
+        default: 0
+    },
     createdAt: {
         type: Date,
-        default: Date.now},
+        default: Date.now
+    },
     comments: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Comment"}]
+        ref: "Comment"
+    }]
 });
 
 const model = mongoose.model("Video", VideoSchema);

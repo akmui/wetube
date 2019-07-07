10cdf3f9aa51942fd0dab23879fd73fd0191ce77
akmui <akmui0912@gmail.com>
2019-07-07 22:06:59 +0900

    7/7

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,9 @@
+import passport from "passport";
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 }
   } = req;
@@ -9,17 +11,24 @@ export const postJoin = (req, res) => {
     res.status(400);
     res.render("join", { pageTitle: "Join" });
   } else {
-    // To do: Register User
-    // To do: Log user in
-    res.redirect(routes.home);
+    try {
+      const user = await User({
+        name,
+        email
+      });
+      await User.register(user, password);
+      next();
+    } catch (error) {
+      console.log(error);
+    }
   }
-};
-
+}
 
 export const getLogin = (req, res) => res.render("login", { pageTitle: "Log in" });
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
-}
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home
+});
 
 export const logout = (req, res) => {
   // To do: Process Log out
--- middlewares.js
+++ middlewares.js
@@ -8,10 +8,8 @@ const multerVideo = multer({ dest: "uploads/videos/" });
 export const localMiddleWare = (req, res, next) => {
     res.locals.siteName = "Wetube";
     res.locals.routes = routes;
-    res.locals.user = {
-        isAuthenticated: false,
-        id: 1
-    }
+    res.locals.user = req.user || null;
+    console.log(req.user);
     next();
 }
 export const uploadVideo = multerVideo.single("videoFile");

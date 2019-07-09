341c18967ed7dcb3c21159848b924afefe59f455
akmui <akmui0912@gmail.com>
2019-07-09 20:33:54 +0900

    7/8

--- middlewares.js
+++ middlewares.js
@@ -11,5 +11,22 @@ export const localMiddleWare = (req, res, next) => {
     res.locals.user = req.user || null;
     console.log(req.user);
     next();
-}
+};
+
+export const onlyPublic = (req, res, next) => {
+    if (req.user) {
+        res.redirect(routes.home);
+    } else {
+        next();
+    }
+};
+
+export const onlyPrivate = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect(routes.home);
+    }
+};
+
 export const uploadVideo = multerVideo.single("videoFile");
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,12 +3,13 @@ import routes from "../routes";
 import {
     users, userDetail, editProfile, changePassword
 } from "../controllers/userController";
+import { onlyPrivate } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/", users);
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;

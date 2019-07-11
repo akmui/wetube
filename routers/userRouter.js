22ac53ec4c750ca07bc84c31cc0dc562e018da13
akmui <akmui0912@gmail.com>
2019-07-11 23:35:46 +0900

    7/11 2

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,14 +1,14 @@
 import express from "express";
 import routes from "../routes";
 import {
-    users, userDetail, editProfile, changePassword
+    users, userDetail, changePassword, getEditProfile
 } from "../controllers/userController";
 import { onlyPrivate } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/", users);
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 

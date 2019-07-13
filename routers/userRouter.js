52e3b113776368628be73a0fbba4f858616c157f
akmui <akmui0912@gmail.com>
2019-07-13 11:06:13 +0900

    7/13 1

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import routes from "../routes";
 import {
-    users, userDetail, changePassword, getEditProfile, postEditProfile
+    users, userDetail, changePassword, getEditProfile, postEditProfile, getChangePassword, postChangePassword
 } from "../controllers/userController";
 import { onlyPrivate, uploadAvatar } from "../middlewares";
 
@@ -11,7 +11,8 @@ userRouter.get("/", users);
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
 
-userRouter.get(routes.changePassword, onlyPrivate, changePassword);
+userRouter.get(routes.changePassword, onlyPrivate, getChangePassword);
+userRouter.post(routes.changePassword, onlyPrivate, postChangePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;

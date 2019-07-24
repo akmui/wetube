f12b707bf9864f90c4a59b3a22b2364f26b42d01
akmui <akmui0912@gmail.com>
2019-07-25 01:51:35 +0800

    7/24 3

--- src/init.js
+++ src/init.js
@@ -9,5 +9,8 @@ import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+const handleListening = () => {
+    console.log(`Listening on: http://localhost:${PORT}`);
+    this.address().port, app.settings.env);
+}
 app.listen(PORT, handleListening);

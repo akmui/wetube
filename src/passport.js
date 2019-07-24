2dcf76dd385056be96e06cb7da14747ab9e164b3
akmui <akmui0912@gmail.com>
2019-07-25 01:44:01 +0800

    7/24

--- src/passport.js
+++ src/passport.js
@@ -9,9 +9,7 @@ passport.use(User.createStrategy());
 passport.use(new GithubStrategy({
     clientID: process.env.GH_ID,
     clientSecret: process.env.GH_SECRET,
-    callbackURL: process.env.PRODUCTION
-        ? `https://nameless-sea-10763.herokuapp.com${routes.githubCallback}`
-        : `http://localhost:4000${routes.githubCallback}`
+    callbackURL: process.env.PRODUCTION ? `https://nameless-sea-10763.herokuapp.com${routes.githubCallback}` : `http://localhost:4000${routes.githubCallback}`
 }, githubLoginCallback));
 
 passport.use(new FacebookStrategy({
